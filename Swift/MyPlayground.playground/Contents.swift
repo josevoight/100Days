97f0740379a7ae0589a0010232e4ff2de7057299
voight <josevoight@gmail.com>
2021-01-20 23:27:19 -0800

    Update Contents.swift
    more coding and learning about swift

--- Swift/MyPlayground.playground/Contents.swift
+++ Swift/MyPlayground.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
 
 var str = "Hello, playground"
+str = "Goodbye"
+
+
+// create a var that stores age
+var age = 38
+
+//var population
+var population = 8_000_000
+
+// this is an example of how to use multi-line strings
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+// another example
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
