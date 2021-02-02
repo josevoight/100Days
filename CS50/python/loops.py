36e520a0362fffde597fadc2abb8e87f8edb89c7
voight <josevoight@gmail.com>
2021-02-01 23:45:33 -0800

    Create loops.py
    for loop examples

---
+++ CS50/python/loops.py
@@ -0,0 +1,18 @@
+# creating a for loop
+for i in [0,1,2,3,4,5]:
+    print(i)
+
+# another example of for loop
+for j in range (6):
+    print(j)
+
+# for loop 
+names = ["Harry", "Ron", "Hermione", "Ginny"]
+
+for name in names:
+    print(name)
+
+name = "Harry"
+
+for character in name:
+    print(character)
