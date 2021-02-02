b45af14172e7158174f405ac513dc9f3e52a61c7
voight <josevoight@gmail.com>
2021-02-01 23:31:00 -0800

    Create lists.py
    example of  a list, sort , append

---
+++ CS50/python/lists.py
@@ -0,0 +1,11 @@
+# Define a list of names
+names = ["Harry", "Ron", "Hermione", "Ginny"]
+
+# add something to the list 
+names.append("Draco")
+
+#sort the names
+names.sort()
+
+# output in order 
+print(names)
