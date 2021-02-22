c07af995165faeecdc880fb1d6216b661e359299
voight <josevoight@gmail.com>
2021-02-22 00:13:25 -0800

    Gulp fixed
    fixed gulp styles and watch

--- CS50/Flexbox/gulpfile.js
+++ CS50/Flexbox/gulpfile.js
@@ -1,15 +1,12 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('style', async function() {
-    gulp.src('css/style.css')
-    .pipe(autoprefixer())
-    .pipe(gulp.dest('build'))
+gulp.task('styles', async function(){
+    return gulp.src('css/style.css')
+   .pipe(autoprefixer('last 2 versions'))
+   .pipe(gulp.dest ('build'));
+});
 
-}); 
-
-gulp.task('watch', async function() {
-    gulp.watch('css/style.css', gulp.series['style']);
-
-
+gulp.task('watch', function(){
+    gulp.watch('css/style.css', gulp.series(['styles']));
 });
